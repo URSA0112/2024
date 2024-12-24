eb77fb4d7ce0ab972c35f1d04705e0e7f5474ec1
URSA0112 <Nimone99@gmail.com>
2024-12-24 20:38:37 +0800

    java 1-100 loop sum multiply

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+var sum = 0;
+for (var i = 1; i <= 100; i++) {
+  if (i % 2 == 0) sum += i;
+}
+console.log(sum); //  between all 1-100 (even) numbers sum = 2550
+
+// multiply ??***//1*2*3*4*5=120
+
+var urjver = 1;
+for (var k = 1; k <= 5; k++) {
+  urjver *= k;
+
+}  console.log(urjver)
+;
