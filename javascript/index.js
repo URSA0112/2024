0111d9bc3a69d6fb519279a03459fc1fba1ac27a
URSA0112 <Nimone99@gmail.com>
2024-12-26 21:08:52 +0800

    list of loof

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,38 @@
-var sum = 0;
-for (var i = 1; i <= 100; i++) {
-  if (i % 2 == 0) sum += i;
-}
-console.log(sum); //  between all 1-100 (even) numbers sum = 2550
+//  1) 1-10 hurteleh toog hevleh
+//for (var i = 1; i <= 10; i++) {
+// var num = 0;
+//if ((num = num + i)) console.log(num);// esvel shuud console.log(i) :))
+//}
 
-// multiply ??***//1*2*3*4*5=120
+// 2) 1-10 hurteleh sondgoi (1,3,5) too hevleh
+//for (var i = 0; i<=10; i++) {
+//if (i%2 !==0) console.log(i) // !==0 (2-t uldegdeltei huvaagddag toonuudig yalgaj ugch baina)
+//}
 
-var urjver = 1;
-for (var k = 1; k <= 5; k++) {
-  urjver *= k;
+// 3) 1-10 hurteleh niilbernuudiig hevleh
+//  ( mur muruur ni (0+1 =1 , =1+2 =3, =3+3 =6, =6+4 =10,... =55)
+//var sum = 0;
+//for (var i = 1; i <=10 ; i++)
+//{if (sum = sum+i) console.log(sum)
+//}
 
-}  console.log(urjver)
-;
+//4) "*" row bolgondoo shirheg ni nemegdej baigaa * hevleh
+//var od =""
+//for (var i=0; i<=5; i++){
+//if (od =od + '*' )
+//console.log(od)}
+//*
+//**
+//***
+//****
+//*****
+
+//5) reverse whit inner loop
+//var row = 5;
+//for (var i = row; i >= 0; i--) {
+//  var num = "";
+//  for (var k = 0; k <= i; k++) {
+//    num += "0";
+//  }
+//  console.log(num);
+//}
