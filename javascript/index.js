d526ca8ea604a5603e500976408f5aed488683b0
URSA0112 <24LP2708@G303-CC0163.local>
2024-12-27 10:33:17 +0800

    copypase

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,39 @@
-var negyes = 9;
-var niityes = negyes*9;
 
-if (negyes >= 9) {
-    console.log('yes-1');
-}
-else {
-    console.log ('1+yes');
-}
+//  1) 1-10 hurteleh toog hevleh
+//for (var i = 1; i <= 10; i++) {
+// var num = 0;
+//if ((num = num + i)) console.log(num);// esvel shuud console.log(i) :))
+//}
 
+// 2) 1-10 hurteleh sondgoi (1,3,5) too hevleh
+//for (var i = 0; i<=10; i++) {
+//if (i%2 !==0) console.log(i) // !==0 (2-t uldegdeltei huvaagddag toonuudig yalgaj ugch baina)
+//}
 
+// 3) 1-10 hurteleh niilbernuudiig hevleh
+//  ( mur muruur ni (0+1 =1 , =1+2 =3, =3+3 =6, =6+4 =10,... =55)
+//var sum = 0;
+//for (var i = 1; i <=10 ; i++)
+//{if (sum = sum+i) console.log(sum)
+//}
 
+//4) "*" row bolgondoo shirheg ni nemegdej baigaa * hevleh
+//var od =""
+//for (var i=0; i<=5; i++){
+//if (od =od + '*' )
+//console.log(od)}
+//*
+//**
+//***
+//****
+//*****
 
-
-
-
-//if (niityes >= 63) {console.log('7yes');}
-//if (niityes >= 54) {console.log('6yes');}
+//5) reverse whit inner loop
+//var row = 5;
+//for (var i = row; i >= 0; i--) {
+//  var num = "";
+//  for (var k = 0; k <= i; k++) {
+//    num += "0";
+//  }
+//  console.log(num);
+//}
