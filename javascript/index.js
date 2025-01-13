57e33f01e249d370887a34e852a317277e512470
URSA0112 <Nimone99@gmail.com>
2025-01-13 22:07:28 +0800

    saving  repo 2024 creating new 2025 repo to use

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+// file system 
+import { isUtf8 } from "buffer";
+import fs from "fs"
+
+
+const content = "some content";
+fs.writeFile('.massege.text', content, (err) =>{
+  
+}
+
+)
+
+;
--- javascript/npm.js
+++
@@ -1,9 +0,0 @@
-const fs = require("node:fs");
-const content = " some content";
-fs.writeFile("/test.txt", content, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    // success
-  }
-});
